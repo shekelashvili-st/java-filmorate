d8cf81fb3bd1ee8c4e1e3bca4961a9e8e565a87c
shekelashvili-st <sergey200004@gmail.com>
2025-04-20 19:07:04 +0300

    Add initial error handlers

---
+++ src/main/java/ru/yandex/practicum/filmorate/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.IdNotFoundException;
+import ru.yandex.practicum.filmorate.model.ApiError;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleIdNotFound(IdNotFoundException e) {
+        return new ApiError(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiError handleUncaught(Throwable e) {
+        return new ApiError(e.getMessage());
+    }
+}
