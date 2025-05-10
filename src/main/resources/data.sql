629e185b518ca4f134a2da13da3bd00b1ab718be
shekelashvili-st <sergey200004@gmail.com>
2025-05-10 17:57:25 +0300

    Fix database ER diagram, add initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO ratings(name)
+VALUES ('G'),
+    ('PG'),
+    ('PG-13'),
+    ('R'),
+    ('NC-17');
+
+INSERT INTO genres(name)
+VALUES ('Комедия'),
+    ('Драма'),
+    ('Мультфильм'),
+    ('Триллер'),
+    ('Документальный'),
+    ('Боевик');
